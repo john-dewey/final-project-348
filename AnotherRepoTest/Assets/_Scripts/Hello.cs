9c30bfcf76611354bd36dc6dfd32a6510c910f3f
WorseHabits <117957010+WorseHabits@users.noreply.github.com>
2024-03-19 10:27:18 -0400

    cahnges

--- AnotherRepoTest/Assets/_Scripts/Hello.cs
+++ AnotherRepoTest/Assets/_Scripts/Hello.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Testing : MonoBehaviour
+public class Hello : MonoBehaviour
 {
+    //hello
     // Start is called before the first frame update
     void Start()
     {
