930d703bcfef0fbbcaadb27fef6902bb5972f2c0
WorseHabits <117957010+WorseHabits@users.noreply.github.com>
2024-03-28 13:04:31 -0400

    Animation additions

--- AnotherRepoTest/Assets/_Scripts/PlayerController.cs
+++ AnotherRepoTest/Assets/_Scripts/PlayerController.cs
@@ -7,79 +7,82 @@ public class PlayerController : MonoBehaviour
     private float _speed = 10.0f;
     private bool _isGrounded;
     private Transform _transform;
-    private Vector3 _currentPosition;
     private Rigidbody _rb;
+    private Animator _animator;
 
 
     void Start()
     {
         _transform = gameObject.GetComponent<Transform>();
-
         _rb = gameObject.GetComponent<Rigidbody>();
+        _animator = gameObject.GetComponent<Animator>();
 
         this._isGrounded = false;
-
-        _currentPosition = _transform.position;
     }
 
     void Update()
     {
-        //Jump
+        // Jump
         if (Input.GetButton("Jump") && _isGrounded)
         {
             Jump();
             this._isGrounded = false;
         }
 
-        //Move
-        Move();
+        else if(Input.GetAxis("Horizontal") != 0){
+
+            Move();
+        }
+        else{
+            
+            Idle();
+        }
+        
     }
 
+    void Idle(){
+
+        _animator.Play("idle");
+    }
     void Jump()
     {
         float thrust = 30.0f;
 
         if (Physics.gravity.y < 0)
         {
+            _animator.Play("jump");
             thrust *= 1;
         }
         else
         {
+
+            _animator.Play("jump");
             thrust *= -1;
         }
 
         _rb.AddForce(transform.up * _speed * thrust);
-
-
     }
 
     void Move()
     {
-        // Set the velcoity to move 10 meters per second
         float translation = Input.GetAxis("Horizontal") * _speed * Time.deltaTime;
-        
-        //Uodate translation vector
+        _animator.Play("walk");
         transform.Translate(translation, 0, 0);
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        //is grounded if touching the ground
         if (collision.gameObject.tag == "Ground")
         {
             this._isGrounded = true;
         }
-
     }
 
     void OnCollisionExit(Collision collision)
     {
-        //is not grounded if leaving the ground
         if (collision.gameObject.tag == "Ground")
         {
             this._isGrounded = false;
         }
-
     }
 }
-
