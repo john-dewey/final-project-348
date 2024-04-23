a3fe14206aab750e24a385a0ee9bbca1dda4e1e3
WorseHabits <117957010+WorseHabits@users.noreply.github.com>
2024-04-23 09:28:07 -0400

    Angle idling for boss fight

---
+++ AnotherRepoTest/Assets/_Scripts/Enemy/AngelIdle.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngelIdle : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.Play("follow");
+    }
+
+}
