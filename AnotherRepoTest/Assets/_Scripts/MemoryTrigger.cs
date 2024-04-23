7e5b9d6ce0987b90306943c7fcf66da1b81f949b
Sluck <samluckleonard@gmail.com>
2024-04-23 11:21:05 -0400

    that damn loop

--- AnotherRepoTest/Assets/_Scripts/MemoryTrigger.cs
+++ AnotherRepoTest/Assets/_Scripts/MemoryTrigger.cs
@@ -16,27 +16,11 @@ public class MemoryTrigger : MonoBehaviour
     {
         _renderer = other.gameObject.GetComponent<SpriteRenderer>();
         _animator = other.gameObject.GetComponent<Animator>();
-         bool loopExit = false;
 
         if (other.CompareTag("Player") && this.tag == "MemoryPiece")
         {
             memoryPieceScript.TriggerDialouge();
-            loopExit = true;
         }
-
        
-        Animator animator = DialougeBox.GetComponent<Animator>();
-            
-       while (loopExit == true)
-       {
-        // Get the value of the boolean parameter
-        bool isOpen = animator.GetBool("IsOpen");
-        Debug.Log("isOpen" + isOpen);
-        if (isOpen == false)
-        {
-            levelLoader.LoadNextLevel();
-            loopExit = false;
-        } 
-       }
     }
 }
